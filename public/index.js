c0f3993de8988d3fe94663878c58ca7c8fb0545f
Jihoon Kim <kjh618@kaist.ac.kr>
2020-08-18 20:51:33 +0900

    Implement basic player list and song preloading

--- public/index.js
+++ public/index.js
@@ -1,17 +1,39 @@
 const socket = io();
 
-const messages = document.getElementById('messages');
-const form = document.getElementById('form');
-const message = document.getElementById('message');
+const playerList = document.getElementById('playerList');
+function updatePlayerList(players) {
+  playerList.innerHTML = '';
+  for (let player of players) {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(player.name + ': ' + player.score));
+    playerList.appendChild(li);
+  }
+}
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    socket.emit('chat message', message.value);
-    message.value = '';
+
+let playerName = prompt("Enter username: ");
+socket.emit('self connect', playerName);
+if (playerName === 'DJ') {
+  for (let button of document.getElementsByClassName('controlPanelButton')) {
+    button.disabled = false;
+  }
+}
+
+socket.on('player connect', players => {
+  updatePlayerList(players);
 });
 
-socket.on('chat message', msg => {
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(msg));
-    messages.appendChild(li);
+socket.emit('song request', songs => {
+  let numLoaded = 0;
+  function loaded() {
+    numLoaded++;
+    if (numLoaded == songs.length) {
+      document.getElementById('loadingMessage').textContent = 'All songs loaded.'
+    }
+  }
+
+  for (let song of songs) {
+    let audio = new Audio(song);
+    audio.addEventListener('canplaythrough', loaded, false);
+  }
 });
--- server.js
+++ server.js
@@ -6,22 +6,37 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-io.on('connection', socket => {
-  console.log('a user connected');
-
-  socket.on('chat message', msg => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  })
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+  res.sendFile('/public/index.html');
 });
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
+});
+
+let players = [];
+let questions = require('./questions.json');
+
+io.on('connection', socket => {
+  socket.on('self connect', playerName => {
+    let player = players.find(p => p.name === playerName);
+    if (player !== undefined) {
+      player.socketId = socket.id
+    } else {
+      let newPlayer = { name: playerName, socketId: socket.id, score: 0 };
+      players.push(newPlayer);
+    }
+    io.emit('player connect', players);
+
+    console.log(`${playerName} connected`);
+  });
+
+  socket.on('disconnect', () => {
+    let player = players.find(player => player.socketId === socket.id);
+    console.log(`${player.name} disconnected`);
+  });
+
+  socket.on('song request', (clientCallback) => {
+    let songs = questions.map(q => q.question);
+    clientCallback(songs);
+  })
 });
